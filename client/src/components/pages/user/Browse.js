1a4697198b927e7c853de858083327f68b82fa52
Saieed Shafi <saieedalsaleel@gmail.com>
2023-09-05 15:28:26 +0530

    bug fix

--- client/src/components/pages/user/Browse.js
+++ client/src/components/pages/user/Browse.js
@@ -153,7 +153,6 @@ const Browse = () => {
             });
             return null;
           });
-          // console.log("prods : ", products);
           setNoProductMessage(false);
           setBrowseProducts(products);
           setShownProducts(products);
